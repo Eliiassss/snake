bfd137402ad8696317f608f4087f8cb01fe95e44
Eliiassss <111454321+Eliiassss@users.noreply.github.com>
2023-08-23 20:55:52 -0300

    script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let mileiVisible = false;
 
 let lastTime = 0;
 let deltaTime = 0;
-const frameRate = 7; // Menor valor = velocidad más lenta
+const frameRate = 10; // Menor valor = velocidad más lenta
 
 function drawSnake() {
   ctx.fillStyle = "#00FF00";
